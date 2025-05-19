cb94e68a1b967edd215ef4ec20b594e9846d3414
Samuel Ezra Nathan Andrew Sirait <samuelezra2013@gmail.com>
2025-05-19 12:16:03 +0800

    change api to local

--- lib/api/api_constant.dart
+++ lib/api/api_constant.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = "https://backend-klinik-aesthetic-production.up.railway.app/api"; // Ganti dengan URL server Anda jika sudah online.
+  static const String baseUrl = "http://127.0.0.1:8000/api"; // Ganti dengan URL server Anda jika sudah online.
   static const String register = "$baseUrl/register";
   static const String login = "$baseUrl/login";
   static const String logout = "$baseUrl/logout";
@@ -7,7 +7,7 @@ class ApiConstants {
   static const String product = "$baseUrl/produk";
   static const String jadwalDokter = "$baseUrl/jadwal-dokter";
   static const String booking = "$baseUrl/bookingTreatments";
-    static const String bookingTreatment = "$baseUrl/detailBookingTreatments";
+  static const String bookingTreatment = "$baseUrl/detailBookingTreatments";
   static const String dokter = "$baseUrl/dokters";
   static const String beautician = "$baseUrl/beauticians";
   static const String treatment = "$baseUrl/treatments";
